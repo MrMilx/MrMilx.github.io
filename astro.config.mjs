3930b840cc4e437c31dc7f97ba5928524c5dbe2b
Camilo Vargas <123763043+MrMilx@users.noreply.github.com>
2025-08-03 18:39:44 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://MrMilx.github.io',
   vite: {
       plugins: [tailwindcss()],
   },
